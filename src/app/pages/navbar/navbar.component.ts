bb88eee92a50d441e11158b961e617e9b4d8f5e3
Luis Gallegos <luis.gallegos@im-projects.com>
2025-08-04 20:32:50 +0200

    agregar boton para ir a atras (home)

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -5,21 +5,30 @@ import { MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatMenuModule} from '@angular/material/menu';
 import { SessionStorageService } from '../../models/session-storage-service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd  } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatTooltipModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatTooltipModule, CommonModule],
 })
 export class NavbarComponent {
   companyData: CompanyData = createEmptyCompanyData();
   userName: string = '';
+  isHome: boolean = true;
 
-  constructor( private session: SessionStorageService, private router: Router ) { }
+  constructor( private session: SessionStorageService, private router: Router ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isHome = event.urlAfterRedirects === '/home';
+    });
+  }
 
   ngOnInit() {
     this.companyData = this.session.getData();
